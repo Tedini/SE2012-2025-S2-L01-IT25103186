ca1fe45d4ef6a02659214fb3ef181790f0fbc6dc
Tedini <it25103186@my.sliit.lk>
2026-07-22 12:26:08 +0530

    Q2 is partially coded

---
+++ Marks.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Marks {
+	public static void main(String[] args){
+
+public static menu(){
+
+System.out.println("^^".repeat(40));
+System.out.println(" ".repeat(17));
+System.out.println("MENU");
+System.out.println("^^".repeat(40));
+
+System.out.println("Press the number 1-6 to choose options given below.\n");
+System.out.println("1.) Enter Marks for subjects");
+System.out.println("2.) Update a student's mark");
+System.out.println("3.) View AVERAGE of each student");
+System.out.println("4.) View AVERAGE of each subject");
+System.out.println("5.) View TOTAL marks of each student");
+System.out.println("6.) EXIT\n");
+
+System.out.println("^^".repeat(40));
+}
+
+public static addMarks(int num){
+for(int n = 0; n < num; n++){
+
+}
+
+}
+
+System.out.println("_".repeat(40));
+System.out.println(" ".repeat(15));
+System.out.println("QUESTION 2");
+System.out.println("_".repeat(40));
+
+
+
+	}
+
+}
